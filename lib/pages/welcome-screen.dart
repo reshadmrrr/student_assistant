dee04c8feafa4c7db5d7aa8b79bbdb7d0c27e940
reshadmrrr <reshadmrrr@gmail.com>
2020-08-15 16:30:26 +0600

    all done!

--- lib/pages/welcome-screen.dart
+++ lib/pages/welcome-screen.dart
@@ -15,65 +15,62 @@ class WelcomeScreen extends StatelessWidget {
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Flexible(
-              child: Hero(
-                tag: 'logo',
-                child: CircleAvatar(
-                  radius: width * 0.3,
-                  backgroundImage: AssetImage(kAppLogo),
-                ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Flexible(
+            child: Hero(
+              tag: 'logo',
+              child: CircleAvatar(
+                radius: width * 0.3,
+                backgroundImage: AssetImage(kAppLogo),
               ),
             ),
-            SizedBox(height: height * 0.025),
-            Flexible(
-              child: Hero(
-                tag: 'app-title',
-                child: Image.asset(kAppTitle),
-              ),
+          ),
+          SizedBox(height: height * 0.025),
+          Flexible(
+            child: Hero(
+              tag: 'app-title',
+              child: Image.asset(kAppTitle),
             ),
-            SizedBox(height: height * 0.1),
-            Text("new?"),
-            Hero(
-              tag: 'join-us',
-              child: RoundedRaisedButton(
-                width: width,
-                height: height,
-                title: "Join Us",
-                color: klightBlue,
-                onPressed: () =>
-                    Navigator.pushNamed(context, RegistrationPage.id),
-              ),
+          ),
+          SizedBox(height: height * 0.1),
+          Text("new?"),
+          Hero(
+            tag: 'join-us',
+            child: RoundedRaisedButton(
+              width: width,
+              height: height,
+              title: "Join Us",
+              color: klightBlue,
+              onPressed: () =>
+                  Navigator.pushNamed(context, RegistrationPage.id),
             ),
-            SizedBox(height: height * 0.02),
-            Text("already have an account?"),
-            Hero(
-              tag: 'login',
-              child: RoundedRaisedButton(
-                width: width,
-                height: height,
-                title: "Login",
-                color: klightred,
-                onPressed: () => Navigator.pushNamed(context, LoginScreen.id),
-              ),
+          ),
+          SizedBox(height: height * 0.02),
+          Text("already have an account?"),
+          Hero(
+            tag: 'login',
+            child: RoundedRaisedButton(
+              width: width,
+              height: height,
+              title: "Login",
+              color: klightred,
+              onPressed: () => Navigator.pushNamed(context, LoginScreen.id),
             ),
-            SizedBox(height: height * 0.02),
-            Hero(
-              tag: 'yo',
-              child: RoundedRaisedButton(
-                width: width,
-                height: height,
-                title: "Temporary OTP",
-                color: Colors.deepPurple,
-                onPressed: () => Navigator.pushNamed(context, OTPScreen.id),
-              ),
+          ),
+          SizedBox(height: height * 0.02),
+          Hero(
+            tag: 'yo',
+            child: RoundedRaisedButton(
+              width: width,
+              height: height,
+              title: "Temporary OTP",
+              color: Colors.deepPurple,
+              onPressed: () => Navigator.pushNamed(context, OTPScreen.id),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
